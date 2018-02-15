cb3acbf8d1c1e3f625095f0c1344bb920532aa1c
Unwana Essien <unwana.essien@outlook.com>
2018-02-15 22:40:11 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React_Gsap_Animation
-Created with CodeSandbox
---
+++ src/Bars.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+export const MenuBtn = props =>
+  <svg xmlns="http://www.w3.org/2000/svg" width="279.979" height="160" style={{
+    isolation:
+    "isolate"
+  }} viewBox="0 0 279.979 160">
+    <defs>
+      <clippath id="_clipPath_NiH8wuP35EdBJy8zilor9vjFUxhgKL4S">
+        <rect width="279.979" height="160" />
+      </clippath>
+    </defs>
+    <g clipPath="url(#_clipPath_NiH8wuP35EdBJy8zilor9vjFUxhgKL4S)" stroke={props.color}
+      strokeLinecap="square" strokeMiterlimit="3" strokeWidth="20">
+      <line id="topBar" x1="9.989" x2="269.989" y1="10" y2="10" vectorEffect="non-scaling-stroke"
+      />
+      <line id="midBar" x1="9.989" x2="269.989" y1="80" y2="80" vectorEffect="non-scaling-stroke"
+      />
+      <line id="botBar" x1="9.989" x2="269.989" y1="150" y2="150" vectorEffect="non-scaling-stroke"
+      />
+    </g>
+  </svg>
